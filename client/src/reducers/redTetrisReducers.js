331206b20838ad3a70d8a6a0720a62b58f74801a
Mateo Greil <mateo.greil@gmail.com>
2019-08-01 16:57:02 +0200

    comment reducers

--- client/src/reducers/redTetrisReducers.js
+++ client/src/reducers/redTetrisReducers.js
@@ -1,6 +1,13 @@
 import { combineReducers } from 'redux'
 import move from './move.js'
 
+
+/*
+**  combineReducers permet simplement de combiner pleins de reducers ensemble,
+**  ici c'est inutile, je n'en ai qu'un. Mais c'est parce que je sais tres bien qu'il
+**  y en aura bientot d'autres. Il suffira d'ajouter ici le nom de ton reducer.
+*/
+
 const redTetrisReducers = combineReducers({
   move
 })
