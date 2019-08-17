ac66d8585a323ba5efbb05afdbdc44d52ddad304
Mateo GREIL <mgreil@e1r4p23.42.fr>
2019-08-17 14:59:13 +0200

    ajout d'un message si aucun nom d'utilisateur et de room n'est mis dans l'URL

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import './App.css'
 
 import GameBoardContainer from './containers/GameBoardContainer'
+import Popup from './components/Popup/Popup'
+import { username, gameName } from './socketListener'
+
+
 
 function App() {
+  if (username == null || gameName == null)
+    return (<div>Please, select an username and a room name</div>)
   return (
     <div className="App">
       <GameBoardContainer/>
