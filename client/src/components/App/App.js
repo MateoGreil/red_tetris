589d5318112d3d7c8822e0d5928a3d687cff8f8c
Mateo Greil <mateo.greil@gmail.com>
2019-08-23 12:28:02 +0200

    ajout ecran daccueil "

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css'
 
 import GameBoardContainer from '../../containers/GameBoardContainer'
 import LeftPanelContainer from '../../containers/LeftPanelContainer';
 import RightPanelContainer from '../../containers/RightPanelContainer';
 
+
+
+function MainMenu() {
+  const [username, setUsername] = useState('')
+  const [room, setRoom] = useState('')
+
+  function Submit() {
+    if (room != '' && username != '')
+      document.location.href = '#' + room + '[' + username + ']'
+  }
+
+  return (
+    <div className='MainMenu'>
+      <form onSubmit={Submit} className='Form'>
+        <label>
+          username :
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        </label>
+        <label>
+          room :
+          <input type="text" value={room} onChange={e => setRoom(e.target.value)} />
+        </label>
+        <div>
+          <input type="submit" value="submit"/>
+        </div>
+      </form>
+    </div>
+  )
+}
+
 function App({ error }) {
-  if (error != null)
-    return (<div>{ error }</div>)
+  
+  if (error) {
+    if (error.nb)
+    return (<div>{ error.text }</div>)
+  else if (error.nb === 0)
+    return (<MainMenu/>)
+  }
+  
   return (
     <div className="App">
       <LeftPanelContainer/>
--- client/src/listeners/error.js
+++ client/src/listeners/error.js
@@ -1,9 +1,15 @@
 export default function catchError(socket, getState, username, gameName) {
     socket.on('gameIsBusy', () => {
-		getState().manageGame.error = "ERROR: game is busy"
+    getState().manageGame.error = {
+      text: "ERROR: game is busy",
+      nb: 1
+    }
 	})
 
 	if (!username || !gameName) {
-		getState().manageGame.error = "ERROR: username or room name is invalid or incomplete."
+		getState().manageGame.error = {
+      text: "ERROR: username or room name is invalid or incomplete.",
+      nb: 0
+    }
 	}
 }
