8b818e27b9fd84c9c39a8e1f37ce3cc7120797dc
Mateo GREIL <mgreil@e1r3p15.42.fr>
2019-08-24 15:37:07 +0200

    quit room when gameover

--- server/src/index.js
+++ server/src/index.js
@@ -80,6 +80,7 @@ io.sockets.on('connection', function(client) {
     client.on('gameOver', () => {
       player.gameOver = true
       io.to(player.gameName).emit('players', games[player.gameName].players)
+      client.leave(player.gameName)
     })
 
     client.on('array', array => {
