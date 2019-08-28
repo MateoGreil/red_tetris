521fc0566e697cb6a76d4831f05c4d6dc2fb50f9
Mateo Greil <mateo.greil@gmail.com>
2019-08-28 14:51:56 +0200

    ajout common errors

--- src/client/listeners/error.js
+++ src/client/listeners/error.js
@@ -1,32 +1,14 @@
 const {
-  CONNECTION,
-  DISCONNECT,
-
-  START,
-  RESTART,
-  GAMEOVER,
-  GAME_BUSY,
-
-  PLAYERS,
-
-  NEW_PIECE,
-  ADD_ROW,
-  ARRAY,
-  SCORE
+  GAME_BUSY
 } = require('../../common/eventSocket')
+const errors = require('../../common/errors')
 
 export default function catchError(socket, getState, username, gameName) {
     socket.on(GAME_BUSY, () => {
-    getState().manageGame.error = {
-      text: "ERROR: game is busy",
-      nb: 1
-    }
+    getState().manageGame.error = errors.GAME_BUSY
 	})
 
 	if (!username || !gameName) {
-		getState().manageGame.error = {
-      text: "ERROR: username or room name is invalid or incomplete.",
-      nb: 0
-    }
+		getState().manageGame.error = errors.INVALID_ENTRY
 	}
 }
---
+++ src/common/errors.js
@@ -0,0 +1,12 @@
+const errors = {
+  INVALID_ENTRY: {
+    text: "ERROR: username or room name is invalid or incomplete.",
+    nb: 0
+  },
+  GAME_BUSY: {
+    text: "ERROR: game is busy",
+    nb: 1
+  }
+}
+
+module.exports = errors
